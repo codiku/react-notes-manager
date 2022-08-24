892ed8c36438ed814c75fbd1f5291fa3a168763a
codiku-dev <codiku.dev@gmail.com>
2022-08-24 21:14:45 +0200

    wi

--- src/components/field-error/index.jsx
+++ src/components/field-error/index.jsx
@@ -1,14 +1,4 @@
+import s from "./style.module.css";
 export function FieldError({ msg }) {
-  return (
-    <div
-      style={{
-        width: "100%",
-        marginTop: "0.25rem",
-        fontSize: ".875em",
-        color: "#dc3545",
-      }}
-    >
-      {msg}
-    </div>
-  );
+  return <div className={s.container}>{msg}</div>;
 }
