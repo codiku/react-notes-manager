8f20f0da2537eb0ac938149d439a7c862afbf227
codiku-dev <111140836+codiku-dev@users.noreply.github.com>
2022-10-24 15:06:31 +0200

    wip

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,22 @@
+import { NoteAPI } from "api/note";
 import { Header } from "components/Header/Header";
+import { useEffect } from "react";
+import { useDispatch } from "react-redux";
 import { Outlet } from "react-router-dom";
+import { setNoteList } from "store/notes/notes-slice";
 
 export function App() {
+  const dispatch = useDispatch();
+
+  async function fetchNotes() {
+    const noteList = await NoteAPI.fetchAll();
+    dispatch(setNoteList(noteList));
+  }
+
+  useEffect(() => {
+    fetchNotes();
+  }, []);
+
   return (
     <div>
       <Header />
--- src/store/index.js
+++ src/store/index.js
@@ -1,5 +1,8 @@
 import { configureStore } from "@reduxjs/toolkit";
+import { notesReducer } from "store/notes/notes-slice";
 
 export const store = configureStore({
-  reducer: {},
+  reducer: {
+    notesSlice: notesReducer,
+  },
 });
