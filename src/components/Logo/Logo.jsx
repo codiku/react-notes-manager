f0bd55c1faf2b30f8c3083a6e331ad16ff0a4632
codiku-dev <111140836+codiku-dev@users.noreply.github.com>
2022-10-24 12:56:01 +0200

    Update Logo.jsx

--- src/components/Logo/Logo.jsx
+++ src/components/Logo/Logo.jsx
@@ -1,8 +1,8 @@
 import s from "./style.module.css";
-export function Logo({ image, title, subtitle, onClick }) {
+export function Logo({ image, title, subtitle }) {
   return (
     <>
-      <div onClick={onClick} className={s.container}>
+      <div className={s.container}>
         <img className={s.img} src={image} alt="logo" />
         <div className={s.logo_txt}>{title}</div>
       </div>
