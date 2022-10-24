6ba2f5026b58d216c53387da3b08d0c9234c994e
codiku-dev <111140836+codiku-dev@users.noreply.github.com>
2022-10-24 13:32:54 +0200

    wip

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,10 @@
+import { Header } from "components/Header/Header";
 import { Outlet } from "react-router-dom";
 
 export function App() {
   return (
     <div>
-      React Redux Starter :-)
+      <Header />
       <Outlet />
     </div>
   );
