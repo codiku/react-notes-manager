e9ebdcd3442ba271da46568b4d8fdfef642aeb3e
codiku-dev <111140836+codiku-dev@users.noreply.github.com>
2022-10-29 14:19:33 +0200

    wip

--- src/containers/NoteList/NoteList.jsx
+++ src/containers/NoteList/NoteList.jsx
@@ -14,7 +14,7 @@ export function NoteList() {
             subtitle={note.created_at}
             content={note.content}
             onClickTrash={() => alert("onClickTrash()")}
-            onClickCard={() => navigate("note/" + note.id)}
+            onClick={() => navigate("note/" + note.id)}
           />
         </div>
       ))}
--- src/pages/Note/Note.jsx
+++ src/pages/Note/Note.jsx
@@ -1,3 +1,11 @@
+import { useSelector } from "react-redux";
+import { useParams } from "react-router-dom";
+
 export function Note(props) {
+  const { noteId } = useParams();
+  const note = useSelector((store) =>
+    store.notesSlice.noteList.find((note) => note.id === noteId)
+  );
+  console.log("***", note);
   return <>Note</>;
 }
