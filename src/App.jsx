3e12f1599d76fd10d28ad18085135e5a58f5376c
codiku-dev <111140836+codiku-dev@users.noreply.github.com>
2022-11-28 19:39:03 +0100

    wip

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,10 @@
+import { Header } from "components/Header/Header";
 import { Outlet } from "react-router-dom";
 
 export function App() {
   return (
-    <div>
-      React Redux Starter :-)
+    <div className="container-fluid">
+      <Header />
       <Outlet />
     </div>
   );
