5460dab87406905ae6cb77c4dadc6cfe12fff251
codiku-dev <111140836+codiku-dev@users.noreply.github.com>
2022-11-29 16:33:24 +0100

    wip

---
+++ src/components/TextCard/TextCard.jsx
@@ -0,0 +1,37 @@
+import { useState } from "react";
+import s from "./style.module.css";
+import { Trash as TrashIcon } from "react-bootstrap-icons";
+
+export function TextCard({ title, subtitle, content, onClickTrash, onClick }) {
+  const [isCardHovered, setIsCardHovered] = useState(false);
+  const [isTrashHovered, setIsTrashHovered] = useState(false);
+
+  function onClickTrash_(e) {
+    onClickTrash();
+    e.stopPropagation();
+  }
+  return (
+    <div
+      onClick={onClick}
+      onMouseEnter={() => setIsCardHovered(true)}
+      onMouseLeave={() => setIsCardHovered(false)}
+      className={`card ${s.container}`}
+      style={{ borderColor: isCardHovered ? "#0d6efd" : "transparent" }}
+    >
+      <div className="card-body">
+        <div className={s.title_row}>
+          <h5 className="card-title">{title}</h5>
+          <TrashIcon
+            size={20}
+            onMouseEnter={() => setIsTrashHovered(true)}
+            onMouseLeave={() => setIsTrashHovered(false)}
+            style={{ color: isTrashHovered ? "#FF7373" : "#b8b8b8" }}
+            onClick={onClickTrash_}
+          />
+        </div>
+        <h6 className="card-subtitle mb-2 text-muted">{subtitle}</h6>
+        <p className={`card-text ${s.text_content}`}>{content}</p>
+      </div>
+    </div>
+  );
+}
--- src/pages/NoteBrowse/NoteBrowse.jsx
+++ src/pages/NoteBrowse/NoteBrowse.jsx
@@ -1,3 +1,15 @@
+import { TextCard } from "components/TextCard/TextCard";
+
 export function NoteBrowse(props) {
-  return <>NoteBrowse</>;
+  return (
+    <>
+      <TextCard
+        title="azeazeaz"
+        subtitle="01/01/22"
+        content=" akzejakzeja jaekajzekaje jaekjazkejazkejakejazejazkejakzejazkej jaekajekajek akzejakzeja jaekajzekaje jaekjazkejazkejakejazejazkejakzejazkej jaekajekajekakzejakzeja jaekajzekaje jaekjazkejazkejakejazejazkejakzejazkej jaekajekajekakzejakzeja jaekajzekaje jaekjazkejazkejakejazejazkejakzejazkej jaekajekajek akzejakzeja jaekajzekaje jaekjazkejazkejakejazejazkejakzejazkej jaekajekajek"
+        onClickTrash={() => alert("Click trash")}
+        onClick={() => alert("Click note")}
+      />
+    </>
+  );
 }
