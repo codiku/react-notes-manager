0c2222b474e9b05677a0910c15b8f6d938f54a03
codiku-dev <111140836+codiku-dev@users.noreply.github.com>
2022-11-30 18:34:14 +0100

    wip

--- src/components/NoteForm/NoteForm.jsx
+++ src/components/NoteForm/NoteForm.jsx
@@ -24,8 +24,8 @@ export function NoteForm({
   onSubmit,
 }) {
   const [formValues, setFormValues] = useState({
-    title: note?.title,
-    content: note?.content,
+    title: note?.title || "",
+    content: note?.content || "",
   });
   const [formErrors, setFormErrors] = useState({
     title: note?.title ? undefined : "",
