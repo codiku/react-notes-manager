e8f4553ff58ba492bd914834194ba5e7629697d0
codiku-dev <111140836+codiku-dev@users.noreply.github.com>
2022-11-30 18:26:30 +0100

    wip

--- src/pages/Note/Note.jsx
+++ src/pages/Note/Note.jsx
@@ -1,16 +1,23 @@
+import { NoteAPI } from "api/note-api";
 import { NoteForm } from "components/NoteForm/NoteForm";
 import { useState } from "react";
-import { useSelector } from "react-redux";
+import { useDispatch, useSelector } from "react-redux";
 import { useParams } from "react-router-dom";
+import { updateNote } from "store/note/note-slice";
 
 export function Note(props) {
   const [isEditable, setIsEditable] = useState(false);
+  const dispatch = useDispatch();
   const { noteId } = useParams();
   const note = useSelector((store) =>
     store.NOTE.noteList.find((note) => note.id === noteId)
   );
 
-  function submit() {}
+  async function submit(formValues) {
+    const updatedNote = await NoteAPI.update({ ...formValues, id: note.id });
+    dispatch(updateNote(updatedNote));
+    setIsEditable(false);
+  }
   return (
     <>
       {note && (
