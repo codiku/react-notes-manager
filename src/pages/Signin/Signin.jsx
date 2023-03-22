b96f0b13e8dfab4f09066172c8fc2f03abcc2f45
codiku-dev <111140836+codiku-dev@users.noreply.github.com>
2023-03-22 14:19:35 +0100

    wip

--- src/pages/Signin/Signin.jsx
+++ src/pages/Signin/Signin.jsx
@@ -1,3 +1,22 @@
+import { ButtonPrimary } from "components/ButtonPrimary/ButtonPrimary";
+import { Link } from "react-router-dom";
+import s from "./style.module.css";
 export function Signin() {
-  return <>Signin</>;
+  const form = (
+    <div className={s.formContainer}>
+      <h2 className={s.title}>
+        Signin <br />
+        to access your team notes
+      </h2>
+      <form className={s.formGroup}>
+        <input />
+        <input />
+        <ButtonPrimary className={s.button}>Sign in!</ButtonPrimary>
+        <span>
+          Don't have an account yet ? <Link>Signup</Link>
+        </span>
+      </form>
+    </div>
+  );
+  return <>{form}</>;
 }
