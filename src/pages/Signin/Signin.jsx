cb18899dba753fb35f5d02e0dc286c7270206a18
codiku-dev <111140836+codiku-dev@users.noreply.github.com>
2023-03-23 10:43:21 +0100

    wip

--- src/pages/Signin/Signin.jsx
+++ src/pages/Signin/Signin.jsx
@@ -1,20 +1,34 @@
 import { ButtonPrimary } from "components/ButtonPrimary/ButtonPrimary";
 import { Input } from "components/Input/Input";
 import { AuthLayout } from "layouts/AuthLayout/AuthLayout";
+import { useState } from "react";
 import { Link } from "react-router-dom";
 import s from "./style.module.css";
 
 export function Signin() {
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const submit = (e) => {
+    e.preventDefault();
+    console.log("submited ", email, password);
+  };
+  console.log(email, password);
   const form = (
     <div className={s.formContainer}>
       <h2 className={s.title}>
         Signin <br />
         to access your team notes
       </h2>
-      <form className={s.formGroup}>
-        <Input placeholder={"Email"} />
-        <Input placeholder={"Password"} type="password" />
-        <ButtonPrimary className={s.button}>Sign in!</ButtonPrimary>
+      <form onSubmit={submit} className={s.formGroup}>
+        <Input placeholder={"Email"} onTextChange={setEmail} />
+        <Input
+          placeholder={"Password"}
+          type="password"
+          onTextChange={setPassword}
+        />
+        <ButtonPrimary type="submit" className={s.button}>
+          Sign in!
+        </ButtonPrimary>
         <span>
           Don't have an account yet ? <Link to={"/signup"}>Signup</Link>
         </span>
