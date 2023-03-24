ef21d81d7cde69a18697386318708f9034e3af58
codiku-dev <111140836+codiku-dev@users.noreply.github.com>
2023-03-24 11:13:30 +0100

    wip

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,16 @@
 import { NoteAPI } from "api/note";
+import { ButtonPrimary } from "components/ButtonPrimary/ButtonPrimary";
 import { Header } from "components/Header/Header";
 import { withAuthRequired } from "hoc/withAuthRequired";
 import { useEffect } from "react";
 import { useDispatch } from "react-redux";
-import { Outlet } from "react-router-dom";
+import { Outlet, useNavigate } from "react-router-dom";
 import { setNoteList } from "store/notes/notes-slice";
+import s from "./style.module.css";
 
 export function App() {
   const dispatch = useDispatch();
-
+  const navigate = useNavigate();
   async function fetchNotes() {
     const noteList = await NoteAPI.fetchAll();
     dispatch(setNoteList(noteList));
@@ -21,6 +23,12 @@ export function App() {
   return (
     <div>
       <Header />
+      <ButtonPrimary
+        className={s.buttonAdd}
+        onClick={() => navigate("/note/new")}
+      >
+        +
+      </ButtonPrimary>
       <div style={{ padding: 50 }}>
         <Outlet />
       </div>
--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -2,10 +2,36 @@ import { Logo } from "components/Logo/Logo";
 import s from "./style.module.css";
 import logoSrc from "assets/images/logo.png";
 import { ButtonPrimary } from "components/ButtonPrimary/ButtonPrimary";
-import { useNavigate } from "react-router-dom";
+import { Link, useNavigate } from "react-router-dom";
+import { useDispatch, useSelector } from "react-redux";
+import { selectUser } from "store/auth/auth-selectors";
+import { AuthAPI } from "api/auth";
+import { setUser } from "store/auth/auth-slice";
 
 export function Header() {
   const navigate = useNavigate();
+  const user = useSelector(selectUser);
+  const dispatch = useDispatch();
+
+  const signout = () => {
+    AuthAPI.signout();
+    dispatch(setUser(null));
+  };
+  const renderAuthProfil = () => {
+    return (
+      <div>
+        <img
+          src={`https://api.dicebear.com/5.x/bottts/svg?seed=${user.email}`}
+          style={{ width: 40 }}
+          className="rounded-circle"
+        />
+        <div> Hello , {user.email}</div>
+        <Link to="#" onClick={signout}>
+          Signout
+        </Link>
+      </div>
+    );
+  };
   return (
     <div className={`row ${s.container}`}>
       <div className="col-xs-12 col-sm-4">
@@ -16,11 +42,7 @@ export function Header() {
           image={logoSrc}
         />
       </div>
-      <div className="col-xs-12 col-sm-8 text-end">
-        <ButtonPrimary onClick={() => navigate("/note/new")}>
-          Add note +
-        </ButtonPrimary>
-      </div>
+      <div className="col-xs-12 col-sm-8 text-end">{renderAuthProfil()}</div>
     </div>
   );
 }
