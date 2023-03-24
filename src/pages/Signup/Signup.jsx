fd6a83d99764a2a5c25e79b26b0b78dc06ec7e35
codiku-dev <111140836+codiku-dev@users.noreply.github.com>
2023-03-24 12:42:32 +0100

    wip

--- src/pages/Signup/Signup.jsx
+++ src/pages/Signup/Signup.jsx
@@ -1,3 +1,61 @@
+import { AuthAPI } from "api/auth";
+import { ButtonPrimary } from "components/ButtonPrimary/ButtonPrimary";
+import { Input } from "components/Input/Input";
+import { AuthLayout } from "layouts/AuthLayout/AuthLayout";
+import { useState } from "react";
+import { Link, useNavigate } from "react-router-dom";
+import s from "./style.module.css";
+import { setUser } from "store/auth/auth-slice";
+import { useDispatch } from "react-redux";
+import { toast } from "utils/sweet-alert";
 export function Signup() {
-  return <>Signup</>;
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const [password2, setPassword2] = useState("");
+  const dispatch = useDispatch();
+  const navigate = useNavigate();
+  const submit = async (e) => {
+    e.preventDefault();
+    if (password === password2) {
+      try {
+        const user = await AuthAPI.signup(email, password);
+        dispatch(setUser(user));
+        await toast("success", "Signup succeed, you are now logged in.");
+        navigate("/");
+      } catch (err) {
+        toast("error", err.message);
+      }
+    } else {
+      toast("error", "Password don't match");
+    }
+  };
+
+  const form = (
+    <div className={s.formContainer}>
+      <h2 className={s.title}>
+        Signup <br />
+        to access your team notes
+      </h2>
+      <form onSubmit={submit} className={s.formGroup}>
+        <Input placeholder={"Email"} onTextChange={setEmail} />
+        <Input
+          placeholder={"Password"}
+          type="password"
+          onTextChange={setPassword}
+        />
+        <Input
+          placeholder={"Password (repeat)"}
+          type="password"
+          onTextChange={setPassword2}
+        />
+        <ButtonPrimary type="submit" className={s.button}>
+          Signup !
+        </ButtonPrimary>
+        <span>
+          Already have an account ? <Link to={"/signin"}>Signin</Link>
+        </span>
+      </form>
+    </div>
+  );
+  return <AuthLayout>{form}</AuthLayout>;
 }
